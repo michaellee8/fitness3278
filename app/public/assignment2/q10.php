1e80495a3ff8ffb2bfc7cc6f30af03a819c43f02
michaellee8 <ckmichael8@gmail.com>
2022-03-31 23:22:04 +0800

    chore: wip

--- app/public/assignment2/q10.php
+++ app/public/assignment2/q10.php
@@ -1,49 +1,48 @@
 <?php
 include("connect.php");
 
-$class_id = mysqli_real_escape_string($con, $_POST["class_ID"]);
+$class_id = mysqli_real_escape_string($con, $_GET["class_ID"]);
 
 // prepare the database query
 // select the branch_ID and name of the branches in the “Tai Po” district.
-$query = "
-SELECT C.class_ID, C.name, C.date, C.capacity, COUNT(E.member_ID) AS num_of_enrollment
+$query_class = "
+SELECT C.class_ID, C.name as class_name, C.date, C.capacity, C.description, I.name as instructor_name
 FROM Class C
-         JOIN Branch B on B.branch_ID = C.branch_ID
-         LEFT JOIN Enrollment E on C.class_ID = E.class_ID
-WHERE B.branch_ID = '$class_id'
-GROUP BY C.class_ID
+         JOIN Instructor I on C.instructor_ID = I.instructor_ID
+WHERE C.class_ID = '$class_id'
 ORDER BY C.class_ID DESC
 ";
 
 // execute the query
-$result = mysqli_query($con, $query) or die("Unable to execute query:" . mysqli_error($con));
+$result_class = mysqli_query($con, $query_class) or die("Unable to execute query:" . mysqli_error($con));
 
 echo "<!DOCTYPE html><html>";
 echo "<head>";
-echo "<title>Question 9_2</title>";
+echo "<title>Question 10</title>";
 echo "</head>";
 echo "<body  align=center>";
-echo "<h3>Q9_2 Answer</h3>";
+echo "<h3>Q10 Answer</h3>";
 
 // display the table
 echo "<table border='1' align='center'>";
 echo "<tr>";
 echo "<td>class_ID</td>";
-echo "<td>name</td>";
+echo "<td>class_name</td>";
 echo "<td>date</td>";
 echo "<td>capacity</td>";
-echo "<td>num_of_enrollment</td>";
+echo "<td>description</td>";
+echo "<td>instructor_name</td>";
 echo "</tr>";
 
 // print data with HTML
-while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-    $id = $row['class_ID'];
+while ($row = mysqli_fetch_array($result_class, MYSQLI_ASSOC)) {
     echo "<tr>";
     echo "<td>" . $row['class_ID'] . "</td>";
-    echo "<td><a href='q10.php?class_ID=$id'>" . $row['name'] . "</a><br></td>";
+    echo "<td>" . $row['class_name'] . "</td>";
     echo "<td>" . $row['date'] . "</td>";
     echo "<td>" . $row['capacity'] . "</td>";
-    echo "<td>" . $row['num_of_enrollment'] . "</td>";
+    echo "<td>" . $row['description'] . "</td>";
+    echo "<td>" . $row['instructor_name'] . "</td>";
     echo "</tr>";
 }
 echo "</table>";
@@ -52,5 +51,5 @@ echo "</body>";
 echo "</html>";
 
 // last step: free the tuple result and close the MySQL database connection
-mysqli_free_result($result);
+mysqli_free_result($result_class);
 mysqli_close($con);
