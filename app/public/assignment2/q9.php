69038774c4f9a964ee46722e251a10ba58fa81f4
michaellee8 <ckmichael8@gmail.com>
2022-03-31 22:59:24 +0800

    feat: q9

--- app/public/assignment2/q9.php
+++ app/public/assignment2/q9.php
@@ -1,17 +1,18 @@
 <?php
 include("connect.php");
 
+$id = mysqli_real_escape_string($con, $_POST["ID"]);
+
 // prepare the database query
 // select the branch_ID and name of the branches in the “Tai Po” district.
 $query = "
-SELECT M.member_ID          as member_ID,
-       M.name               as name,
-       COUNT(R.referrer_ID) as num_of_referee
-FROM Member M
-         JOIN Member R ON M.member_ID = R.referrer_ID
-GROUP BY M.member_ID
-HAVING COUNT(R.referrer_ID) > 2
-ORDER BY M.member_ID DESC
+SELECT C.class_ID, C.name, C.date, C.capacity, COUNT(E.member_ID) AS num_of_enrollment
+FROM Class C
+         JOIN Branch B on B.branch_ID = C.branch_ID
+         LEFT JOIN Enrollment E on C.class_ID = E.class_ID
+WHERE B.branch_ID = '$id'
+GROUP BY C.class_ID
+ORDER BY C.class_ID DESC
 ";
 
 // execute the query
@@ -19,26 +20,30 @@ $result = mysqli_query($con,$query) or die( "Unable to execute query:".mysqli_er
 
 echo "<!DOCTYPE html><html>";
 echo "<head>";
-echo "<title>Question 8</title>";
+echo "<title>Question 9_2</title>";
 echo "</head>";
 echo "<body  align=center>";
-echo "<h3>Q8 Answer</h3>";
+echo "<h3>Q9_2 Answer</h3>";
 
 // display the table
 echo "<table border='1' align='center'>";
 echo "<tr>";
-echo "<td>member_ID</td>";
+echo "<td>class_ID</td>";
 echo "<td>name</td>";
-echo "<td>num_of_referee</td>";
+echo "<td>date</td>";
+echo "<td>capacity</td>";
+echo "<td>num_of_enrollment</td>";
 echo "</tr>";
 
 // print data with HTML
 while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
 {
     echo "<tr>";
-    echo "<td>".$row['member_ID']."</td>";
+    echo "<td>".$row['class_ID']."</td>";
     echo "<td>".$row['name']."</td>";
-    echo "<td>".$row['num_of_referee']."</td>";
+    echo "<td>".$row['date']."</td>";
+    echo "<td>".$row['capacity']."</td>";
+    echo "<td>".$row['num_of_enrollment']."</td>";
     echo "</tr>";
 }
 echo "</table>";
